866ff8a05382ce975b43bb0c235175f4b0f634de
Michal Srb <michal.srb@cesnet.cz>
2011-05-18 16:06:56 +0200

    ipfixcol: template manager API and structures
    Function prototypes and definitions of structures of public API for
    communication with Template Manager.

--- headers/templates.h
+++ headers/templates.h
@@ -1,6 +1,7 @@
 /**
  * \file templates.h
  * \author Radek Krejci <rkrejci@cesnet.cz>
+ * \author Michal Srb <michal.srb@cesnet.cz>
  * \brief Public structures and functions (API) of the ipfixcol's Template
  * Manager
  *
@@ -38,18 +39,99 @@
  *
  */
 
-#ifndef TEMPLATES_H_
-#define TEMPLATES_H_
-
 /**
  * \defgroup templateMngAPI Template Manager API
+ * 
+ * These functions should be used to work with Template Manager. Template
+ * Manager is unique for every Data Manager and is represented by \link 
+ * #ipfix_template_mgr_t ipfix_template_mgr_t structure\endlink. Its job 
+ * is to manage (Options) Templates.
  *
  * \todo ipfix_template_t
- * \todo Template Manager functions
  *
  * @{
  */
+#ifndef IPFIXCOL_TEMPLATES_H_
+#define IPFIXCOL_TEMPLATES_H_
+
+/**
+ * \struct ipfix_template_t
+ * \brief Structure for storing Template Record/Options Template Record
+ */
+struct ipfix_template_t {
+	uint8_t template_type;       /**Type of Template - 0 = Template, 
+	                              * 1 = Options Template */
+	time_t last_transmission;    /**Time of last transmission of Template,
+	                              * UDP only */
+	uint16_t template_id;        /**Template ID */
+	uint16_t field_count;        /**Number of fields in Template Record */
+	uint16_t scope_field_count;  /**Number of scope fields */
+	template_ie_u fields[1];     /**Template fields */
+};
+
+/**
+ * \struct ipfix_template_mgr_t
+ * \brief Template Manager structure. 
+ */
+struct ipfix_template_mgr_t {
+	struct ipfix_template_t **templates;/**array of pointers to Templates */
+	uint16_t max_length;  /**maximum length of array */
+	uint16_t counter;     /**number of templates in array */
+};
+
+/**
+ * \brief Function for adding new templates.
+ *
+ * \param[in]  tm Data Manager specific structure for storing Templates.
+ * \param[in]  template Pointer where new Template Record starts.
+ * \param[in]  type Type of the Template Record. 0 = Template, 1 = Options
+ * Template.
+ * \return 0 on success, negative value if error occurs.
+ */
+int tm_add_template(struct ipfix_template_mgr_t *tm, void *template,
+                    int type);
+
+/**
+ * \brief Function for updating an existing templates.
+ *
+ * \param[in]  tm Data Manager specific structure for storing Templates.
+ * \param[in]  template Pointer where new Template Record starts.
+ * \param[in]  type Type of the Template Record. 0 = Template, 1 = Options
+ * Template.
+ * \return 0 on success, negative value if error occurs.
+ */
+int tm_update_template(struct ipfix_template_mgr_t *tm, void *template, 
+                       int type);
+
+/**
+ * \brief Function for specific Template lookup.
+ *
+ * \param[in]  tm Data Manager specific structure for storing Templates.
+ * \param[in]  template_id ID of the Template that we are interested in.
+ * \return pointer on the Temaplate on success, NULL if there is no such
+ * Temaplate.
+ */
+struct ipfix_template_t *tm_get_template(struct ipfix_template_mgr_t *tm, 
+                                         uint16_t template_id);
+
+/**
+ * \brief Function for removing Temaplates.
+ *
+ * \param[in]  tm Data Manager specific structure for storing Templates.
+ * \param[in]  template_id ID of the Template that we want to remove.
+ * \return 0 on success, negative value otherwise.
+ */
+int tm_remove_template(struct ipfix_template_mgr_t *tm,
+                       uint16_t template_id);
+
+/**
+ * \brief Function for removing all Temaplates.
+ *
+ * \param[in]  tm Data Manager specific structure for storing Templates.
+ * \return 0 on success, negative value otherwise.
+ */
+int tm_remove_all_templates(struct ipfix_template_mgr_t *tm);
+
+#endif /* IPFIXCOL_TEMPLATES_H_ */
 
 /**@}*/
-
-#endif /* TEMPLATES_H_ */
