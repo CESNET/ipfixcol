97266c6f6af105bb24cad785213fb4120a688ead
Radek Krejci <rkrejci@cesnet.cz>
2011-06-08 15:22:26 +0200

    ipfixcol: add simple main source file
    Added really primitive main source file ipfixcol.c just to enable make.

---
+++ src/ipfixcol.c
@@ -0,0 +1,113 @@
+/**
+ * \file ipfixcol.c
+ * \author Radek Krejci <rkrejci@cesnet.cz>
+ * \brief Main body of the ipfixcol
+ *
+ * Copyright (C) 2011 CESNET, z.s.p.o.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of the Company nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * ALTERNATIVELY, provided that this notice is retained in full, this
+ * product may be distributed under the terms of the GNU General Public
+ * License (GPL) version 2 or later, in which case the provisions
+ * of the GPL apply INSTEAD OF those given above.
+ *
+ * This software is provided ``as is, and any express or implied
+ * warranties, including, but not limited to, the implied warranties of
+ * merchantability and fitness for a particular purpose are disclaimed.
+ * In no event shall the company or contributors be liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential
+ * damages (including, but not limited to, procurement of substitute
+ * goods or services; loss of use, data, or profits; or business
+ * interruption) however caused and on any theory of liability, whether
+ * in contract, strict liability, or tort (including negligence or
+ * otherwise) arising in any way out of the use of this software, even
+ * if advised of the possibility of such damage.
+ *
+ */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "../ipfixcol.h"
+
+/** Version number */
+#define VERSION "0.1"
+
+/** Acceptable command-line parameters */
+#define OPTSTRING "dhv:V"
+
+/**
+ * \brief Print program version information
+ * @param progname Name of the program
+ */
+void version (char* progname)
+{
+	printf ("%s: IPFIX Collector capture daemon.\n", progname);
+	printf ("Version: %s, Copyright (C) 2011 CESNET z.s.p.o.\n", VERSION);
+	printf ("See http://www.liberouter.org/ipfixcol/ for more information.\n\n");
+}
+
+/**
+ *
+ * @param progname
+ */
+void help (char* progname)
+{
+	printf ("Usage: %s [-dhV] [-v level]\n", progname);
+	printf ("\t-d        Daemonize\n");
+	printf ("\t-h        Print this help\n");
+	printf ("\t-v level  Print verbose messages up to specified level\n");
+	printf ("\t-V        Print version information\n\n");
+}
+
+int main (int argc, char* argv[])
+{
+	int c;
+	bool daemonize = false;
+	char *progname;
+
+	/* get program name withou execute path */
+	progname = ((progname = strrchr (argv[0], '/')) != NULL) ? (progname + 1) : argv[0];
+
+	/* parse command line parameters */
+	while ((c = getopt (argc, argv, OPTSTRING)) != -1) {
+		switch (c) {
+		case 'd':
+			daemonize = true;
+			break;
+		case 'h':
+			version (progname);
+			help (progname);
+			exit (EXIT_SUCCESS);
+			break;
+		case 'v':
+//			set_msg_level (atoi (optarg));
+			break;
+		default:
+			fprintf (stderr, "Unknown parameter %c", c);
+			help (progname);
+			exit (EXIT_FAILURE);
+			break;
+		}
+	}
+
+	fprintf (stderr, "done :)");
+
+	return (0);
+}
+
