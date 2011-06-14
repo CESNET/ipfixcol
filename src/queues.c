f5cd6fcc7af47c487a5426282ddc3c2ae5c55a15
Radek Krejci <rkrejci@cesnet.cz>
2011-06-14 13:29:24 +0200

    ipfixcol: queue for passing data in ipfixcol core
    ipfixcol core needs two queues (actually ring buffers) for passing
    internal data.
    
    First for passing data from ipfix preprocessor into data manager. There
    is one writer (ipfix preprocessor) and one reader (each data manager has
    its own queue with ipfix preprocessor).
    
    Second is for passing data from data manager into storage plugins. This
    one is used by one writer (data manager) and one or more readers
    (storage plugins). All readerd must be able to read all data written
    into the queue, the data are dequeued after all reading threads read the
    data.
    
    Both these queues (ring buffers) are implemented by ring_buffer
    structure and rbuffer_* manipulating functions.

---
+++ src/queues.c
@@ -0,0 +1,295 @@
+/**
+ * \file queues.c
+ * \author Radek Krejci <rkrejci@cesnet.cz>
+ * \brief Implementation of queues needed by ipfixcol core to pass data.
+ *
+ * Copyright (C) 2011 CESNET, z.s.p.o.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of the Company nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * ALTERNATIVELY, provided that this notice is retained in full, this
+ * product may be distributed under the terms of the GNU General Public
+ * License (GPL) version 2 or later, in which case the provisions
+ * of the GPL apply INSTEAD OF those given above.
+ *
+ * This software is provided ``as is, and any express or implied
+ * warranties, including, but not limited to, the implied warranties of
+ * merchantability and fitness for a particular purpose are disclaimed.
+ * In no event shall the company or contributors be liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential
+ * damages (including, but not limited to, procurement of substitute
+ * goods or services; loss of use, data, or profits; or business
+ * interruption) however caused and on any theory of liability, whether
+ * in contract, strict liability, or tort (including negligence or
+ * otherwise) arising in any way out of the use of this software, even
+ * if advised of the possibility of such damage.
+ *
+ */
+
+#include <unistd.h>
+#include <errno.h>
+
+#include "queues.h"
+#include "commlbr.h"
+
+/**
+ * \brief Initiate ring buffer structure with specified size.
+ *
+ * @param[in] size Size of the ring buffer.
+ * @return Pointer to initialized ring buffer structure.
+ */
+struct ring_buffer* rbuffer_init (unsigned int size)
+{
+	struct ring_buffer* retval = NULL;
+
+	if (size == 0) {
+		VERBOSE (CL_VERBOSE_BASIC, "Size of the ring buffer set to zero.");
+		return (NULL);
+	}
+
+	retval = (struct ring_buffer*) malloc (sizeof(struct ring_buffer));
+	if (retval == NULL) {
+		VERBOSE (CL_VERBOSE_OFF, "Memory allocation failed (%s:%d)", __FILE__, __LINE__);
+		return (NULL);
+	}
+
+	retval->read_offset = 0;
+	retval->write_offset = 0;
+	retval->size = size;
+	retval->data = (struct ipfix_message**) malloc (size * sizeof(struct ipfix_message*));
+	if (retval->data == NULL) {
+		VERBOSE (CL_VERBOSE_OFF, "Memory allocation failed (%s:%d)", __FILE__, __LINE__);
+		free (retval);
+		return (NULL);
+	}
+	retval->data_references = (unsigned int*) calloc (size, sizeof(unsigned int));
+	if (retval->data_references == NULL) {
+		VERBOSE (CL_VERBOSE_OFF, "Memory allocation failed (%s:%d)", __FILE__, __LINE__);
+		free (retval->data);
+		free (retval);
+		return (NULL);
+	}
+
+	if (pthread_mutex_init (&(retval->mutex), NULL) != 0) {
+		VERBOSE (CL_VERBOSE_OFF, "Condition variable init failed (%s:%d)", __FILE__, __LINE__);
+		free (retval->data_references);
+		free (retval->data);
+		free (retval);
+	}
+
+	if (pthread_cond_init (&(retval->cond), NULL) != 0) {
+		VERBOSE (CL_VERBOSE_OFF, "Condition variable init failed (%s:%d)", __FILE__, __LINE__);
+		pthread_mutex_destroy (&(retval->mutex));
+		free (retval->data_references);
+		free (retval->data);
+		free (retval);
+		return (NULL);
+	}
+
+	return (retval);
+}
+
+/**
+ * \brief Add new record into the ring buffer.
+ *
+ * @param[in] rbuffer Ring buffer.
+ * @param[in] record IPFIX message structure to be added into the ring buffer.
+ * @param[in] refcount Initial refference count - number of reading threads.
+ * @return 0 on success, nonzero on error.
+ */
+int rbuffer_write (struct ring_buffer* rbuffer, struct ipfix_message* record, unsigned int refcount)
+{
+	if (rbuffer == NULL || record == NULL || refcount == 0) {
+		VERBOSE (CL_VERBOSE_OFF, "Invalid ring buffer's write parameters.")
+		return (EXIT_FAILURE);
+	}
+
+	if (pthread_mutex_lock (&(rbuffer->mutex)) != 0) {
+		VERBOSE (CL_VERBOSE_OFF, "Mutex lock failed (%s:%d)", __FILE__, __LINE__);
+		return (EXIT_FAILURE);
+	}
+	/* it will be never more than ring buffer size, but just to be sure I'm checking it */
+	while (rbuffer->count >= rbuffer->size) {
+		if (pthread_cond_wait (&(rbuffer->cond), &(rbuffer->mutex)) != 0) {
+			VERBOSE (CL_VERBOSE_OFF, "Condition wait failed (%s:%d)", __FILE__, __LINE__);
+			return (EXIT_FAILURE);
+		}
+	}
+	if (pthread_mutex_unlock (&(rbuffer->mutex)) != 0) {
+		VERBOSE (CL_VERBOSE_OFF, "Mutex unlock failed (%s:%d)", __FILE__, __LINE__);
+		return (EXIT_FAILURE);
+	}
+
+	/*
+	 * I don't need mutex since there is always only one write thread to change
+	 * write_offset and count is changed atomically. Now I also know that there
+	 * is at least one place to store data and if someone will change the count,
+	 * it will be decreased, because I'm the only write thread that increases
+	 * the count.
+	 */
+	rbuffer->data[rbuffer->write_offset] = record;
+	rbuffer->data_references[rbuffer->write_offset] = refcount;
+	rbuffer->write_offset++;
+	__sync_fetch_and_add (&(rbuffer->count), 1); /* atomic rbuffer->count++; */
+
+	/* I did change of rbuffer->count so inform about it other threads (read threads) */
+	if (pthread_cond_signal (&(rbuffer->cond)) != 0) {
+		VERBOSE (CL_VERBOSE_OFF, "Condition signal failed (%s:%d)", __FILE__, __LINE__);
+		return (EXIT_FAILURE);
+	}
+
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * \brief Get pointer to data in ring buffer - its position is specified by
+ * index or by ring buffer's current read offset.
+ *
+ * @param[in] rbuffer Ring buffer.
+ * @param[in] index If (unsigned int)-1, use ring buffer's read offset, else try
+ * to get record from index (if value in index is valid).
+ * @return Read data from specified index (or read offset) or NULL on error.
+ */
+struct ipfix_message* rbuffer_read (struct ring_buffer* rbuffer, unsigned int index)
+{
+	unsigned int req_count;
+
+	if (index == (unsigned int) -1) {
+		/* if no index specified -> read from read_offset, so just 1 record in ring buffer required */
+		req_count = 1;
+	} else {
+		/* we will read from specified index so get required ring buffer's count for the index */
+		req_count = 1 + ((rbuffer->read_offset > index) ? rbuffer->size + index - rbuffer->read_offset : index - rbuffer->read_offset);
+	}
+
+	/* check if the ring buffer is full enough, if not wait (and block) for it */
+	if (pthread_mutex_lock (&(rbuffer->mutex)) != 0) {
+		VERBOSE (CL_VERBOSE_OFF, "Mutex lock failed (%s:%d)", __FILE__, __LINE__);
+		return (NULL);
+	}
+	while (rbuffer->count < req_count) {
+		if (pthread_cond_wait (&(rbuffer->cond), &(rbuffer->mutex)) != 0) {
+			VERBOSE (CL_VERBOSE_OFF, "Condition wait failed (%s:%d)", __FILE__, __LINE__);
+			return (NULL);
+		}
+	}
+	if (pthread_mutex_unlock(&(rbuffer->mutex)) != 0) {
+		VERBOSE (CL_VERBOSE_OFF, "Mutex unlock failed (%s:%d)", __FILE__, __LINE__);
+		return (NULL);
+	}
+
+	/* get data */
+	if (index == (unsigned int) -1) {
+		/* if no index specified -> read from read_offset */
+		return (rbuffer->data[rbuffer->read_offset]);
+	} else {
+		return (rbuffer->data[index]);
+	}
+
+	return (NULL);
+}
+
+/**
+ * \brief Decrease reference counter on specified record in ring buffer.
+ *
+ * Each thread can use this function only once (for each ring buffer run) when
+ * it is done with data from index. Reference count is set to the number of
+ * threads reading data from the ring buffer. The scheme of usage is usually as
+ * follows:
+ *
+ * - rbuffer_read (); <br/>
+ * - do some work with read data; <br/>
+ * - rbuffer_remove_reference ();
+ *
+ * @param[in] rbuffer Ring buffer.
+ * @param[in] index Index of the item in the ring buffer.
+ * @param[in] dofree 1 to free data with 0 references, 0 to lose data by removing
+ * the pointer, but do not free the data.
+ * @return 0 on success, nonzero on error - no reference on item
+ */
+int rbuffer_remove_reference (struct ring_buffer* rbuffer, unsigned int index, int dofree)
+{
+	if (rbuffer->data_references[index] > 0) {
+		/* atomic rbuffer->data_references[index]--; */
+		__sync_fetch_and_sub (&(rbuffer->data_references[index]), 1);
+	} else {
+		return (EXIT_FAILURE);
+	}
+
+	if (pthread_mutex_lock (&(rbuffer->mutex)) != 0) {
+		VERBOSE (CL_VERBOSE_OFF, "Mutex lock failed (%s:%d)", __FILE__, __LINE__);
+		return (EXIT_FAILURE);
+	}
+	/* it will be never less than zero, but just to be sure I'm checking it */
+	if (rbuffer->data_references[rbuffer->read_offset] <= 0) {
+		while ((rbuffer->data_references[rbuffer->read_offset] == 0) && (rbuffer->count > 0)) {
+			if (dofree) {
+				/* free the data */
+				if (rbuffer->data[rbuffer->read_offset]) {
+					if (rbuffer->data[rbuffer->read_offset]->pkt_header) {
+						free (rbuffer->data[rbuffer->read_offset]->pkt_header);
+					}
+					free (rbuffer->data[rbuffer->read_offset]);
+				}
+			}
+			/* move offset pointer in ring buffer */
+			rbuffer->read_offset = (rbuffer->read_offset + 1) % rbuffer->size;
+			__sync_fetch_and_sub (&(rbuffer->count), 1); /* atomic rbuffer->count--; */
+		}
+		if (pthread_mutex_unlock (&(rbuffer->mutex)) != 0) {
+			VERBOSE (CL_VERBOSE_OFF, "Mutex unlock failed (%s:%d)", __FILE__, __LINE__);
+			return (EXIT_FAILURE);
+		}
+	} else {
+		if (pthread_mutex_unlock (&(rbuffer->mutex)) != 0) {
+			VERBOSE (CL_VERBOSE_OFF, "Mutex unlock failed (%s:%d)", __FILE__, __LINE__);
+			return (EXIT_FAILURE);
+		}
+		/* only reference decrease was done, moving rbuffer->read_offset not */
+		return (EXIT_SUCCESS);
+	}
+
+	/* I did change of rbuffer->count so inform about it other threads (mainly write thread) */
+	if (pthread_cond_signal (&(rbuffer->cond)) != 0) {
+		VERBOSE (CL_VERBOSE_OFF, "Condition signal failed (%s:%d)", __FILE__, __LINE__);
+		return (EXIT_FAILURE);
+	}
+
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * \brief Destroy ring buffer structures.
+ *
+ * @param[in] rbuffer Ring buffer to destroy.
+ * @return 0 on success, nonzero on error.
+ */
+int rbuffer_free (struct ring_buffer* rbuffer)
+{
+	if (rbuffer) {
+		if (rbuffer->data_references) {
+			free (rbuffer->data_references);
+		}
+		if (rbuffer->data) {
+			free (rbuffer->data);
+		}
+
+		pthread_cond_destroy (&(rbuffer->cond));
+		pthread_mutex_destroy (&(rbuffer->mutex));
+
+		free (rbuffer);
+	}
+
+	return (EXIT_SUCCESS);
+}
