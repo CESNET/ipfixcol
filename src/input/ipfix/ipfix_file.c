1345c98ea95ac1a1e818996296cfbaf42fca5642
Michal Srb <michal.srb@cesnet.cz>
2011-07-12 11:08:27 +0200

    ipfixcol: FEATURE: IPFIX input plugin now supports asterisk expansion in names of input files.

--- src/input/ipfix/ipfix_file.c
+++ src/input/ipfix/ipfix_file.c
@@ -50,12 +50,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <dirent.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stddef.h>
 #include <errno.h>
+#include <libgen.h>
 #include <libxml/xmlmemory.h>
 #include <libxml/parser.h>
 #include <commlbr.h>
@@ -63,14 +66,167 @@
 #include "ipfixcol.h"
 
 
+#define NUMBER_OF_INPUT_FILES 100
+#define NO_INPUT_FILE         (-2)
+
+
 /* IPFIX input plugin specific "config" structure */
 struct ipfix_config {
 	int fd;                  /* file descriptor */
 	xmlChar *xml_file;       /* input file URI from XML configuration 
-	                          * file */
-	char *file;              /* actual path where to store messages */
-	struct input_info_file *in_info; /* some info about input */
+	                          * file. (e.g.: "file://tmp/ipfix.dump") */
+	char *file;              /* path where to look for IPFIX files. same as
+	                          * xml_file, but without 'file:' */
+	char *dir;               /* directory where to look for ipfix files.
+	                          * basically it is dirname(file) */
+	char *filename;          /* name of the input file. it may contain asterisk
+	                          * (e.g.: "ipfix-2011-03-*.dump) */
+	char *file_copy;         /* auxiliary variable, copy of the "file" for purpose 
+	                          * of basename() */
+	char **input_files;      /* list of all input files */
+	int findex;              /* index to the current file in the list of files */
+	struct input_info_file *in_info; /* info structure about current input file */
 };
+
+
+/* check whether string matches regexp or not 
+ * return 1 if string matches, 0 otherwise */
+static int regexp_asterisk(char *regexp, char *string)
+{
+	static int asterisk = '*';
+	char *asterisk_pos;
+	char *aux_regexp;
+	char *saveptr;
+	char *token;
+	int ok;             /* 1 if string matches regexp, 0 otherwise */
+
+	if ((regexp == NULL) || (string == NULL)) {
+		return 0;
+	}
+
+	if ((asterisk_pos = strchr(regexp, asterisk)) == NULL) {
+		/* this string doesn't contain asterisk... */
+		if (strcmp(regexp, string)) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	/* make copy of original string */
+	aux_regexp = (char *) malloc(strlen(regexp) + 1);
+	if (aux_regexp == NULL) {
+		VERBOSE(CL_VERBOSE_OFF, "Not enough memory");
+		return -1;
+	}
+
+	strcpy(aux_regexp, regexp);
+	
+	int pos = 1; /* we assume that asterisk is in the middle of the string */
+	if (aux_regexp[0] == asterisk) {
+		/* asterisk on the beginning */
+		pos = 0;
+	}
+	if (aux_regexp[strlen(aux_regexp)-1] == asterisk) {
+		/* asterisk on the end of the string */
+		pos = 2;
+	}
+	if (!strcmp(aux_regexp, "*")) {
+		/* there is nothing but asterisk */
+		pos = -1;
+	}
+
+	token = strtok_r(aux_regexp, "*", &saveptr);
+
+	ok = 0;
+	switch (pos) {
+	case (-1):
+		/* there is nothing but asterisk, so it matches. best scenario :) */
+		ok = 1;
+		break;
+	case (0):
+		/* asterisk is on the beginning of the string */
+		if (!strncmp(token, string+(strlen(string)-strlen(token)), strlen(token))) {
+			ok = 1;
+		}
+		break;
+
+	case (1):
+		/* asterisk is in the middle of the string */
+		if (!strncmp(token, string, strlen(token))) {
+			token = strtok_r(NULL, "*", &saveptr);
+			if (!strncmp(token, string+(strlen(string)-strlen(token)), strlen(token))) {
+				ok = 1;
+			}
+		}
+		break;
+
+	case (2):
+		/* asterisk is on the end of the string */
+		if (!strncmp(token, string, strlen(token))) {
+			ok = 1;
+		}
+		break;
+	}
+
+	return ok;
+}
+
+
+static int prepare_input_file(struct ipfix_config *conf)
+{
+	int fd;
+	int ret = 0;
+
+	if (conf->input_files[conf->findex] == NULL) {
+		/* no more input files, we are done */
+		conf->fd = NO_INPUT_FILE;
+		return -1;
+	}
+
+	fd = open(conf->input_files[conf->findex], O_RDONLY);
+	if (fd == -1) {
+		/* input file doesn't exist or we don't have read permission */
+		VERBOSE(CL_VERBOSE_OFF, "Unable to open input file \"%s\"", conf->file);
+		ret = -1;
+	}
+
+	conf->findex += 1;
+	conf->fd = fd;
+	
+	return ret;
+}
+
+static int close_input_file(struct ipfix_config *conf)
+{
+	int ret;
+
+	ret = close(conf->fd);
+	if (ret == -1) {
+		VERBOSE(CL_VERBOSE_OFF, "Error when closing output file");
+		return -1;
+	}
+
+	conf->fd = -1;
+
+	return 0;
+}
+
+
+/* close current input file (if any), and open new one */
+static int next_file(struct ipfix_config *conf)
+{
+	if (conf->fd <= 0) {
+		close_input_file(conf);
+	}
+	prepare_input_file(conf);
+
+	if (conf->fd != NO_INPUT_FILE) {
+		
+	}
+
+	return conf->fd;
+}
 
 
 /*
@@ -79,17 +235,23 @@ struct ipfix_config {
 
 int input_init(char *params, void **config)
 {
-	int fd;
 	struct ipfix_config *conf;
-
+	char **input_files;
 	xmlDocPtr doc;
 	xmlNodePtr cur;
+	struct dirent *entry;
+	struct dirent *result;
+	int ret;
+	int fcounter = 0;
+	int inputf_index = 0;
+	int len;
+	DIR *dir;
 
 
 	/* allocate memory for config structure */
 	conf = (struct ipfix_config *) malloc(sizeof(*conf));
 	if (!conf) {
-		VERBOSE(CL_VERBOSE_OFF, "not enough memory");
+		VERBOSE(CL_VERBOSE_OFF, "Not enough memory");
 		return -1;
 	}
 	memset(conf, '\0', sizeof(*conf));
@@ -98,12 +260,12 @@ int input_init(char *params, void **config)
 	/* try to parse configuration file */
 	doc = xmlReadMemory(params, strlen(params), "nobase.xml", NULL, 0);
 	if (doc == NULL) {
-		VERBOSE(CL_VERBOSE_OFF, "plugin configuration not parsed successfully");
+		VERBOSE(CL_VERBOSE_OFF, "Plugin configuration not parsed successfully");
 		goto err_init;
 	}
 	cur = xmlDocGetRootElement(doc);
 	if (cur == NULL) {
-		VERBOSE(CL_VERBOSE_OFF, "empty configuration");
+		VERBOSE(CL_VERBOSE_OFF, "Empty configuration");
 		goto err_xml;
 	}
 	if (xmlStrcmp(cur->name, (const xmlChar *) "fileReader")) {
@@ -121,7 +283,7 @@ int input_init(char *params, void **config)
 
 	/* check whether we have found "file" element in configuration file */
 	if (conf->xml_file == NULL) {
-		VERBOSE(CL_VERBOSE_OFF, "\"file\" element is missing. no input, "
+		VERBOSE(CL_VERBOSE_OFF, "\"file\" element is missing. No input, "
 		                        "nothing to do");
 		goto err_xml;
 	}
@@ -137,16 +299,6 @@ int input_init(char *params, void **config)
 	conf->file = (char *) conf->xml_file + 5;
 
 
-	/* open IPFIX file */
-	fd = open(conf->file, O_RDONLY);
-	if (fd == -1) {
-		/* input file doesn't exist or we don't have read permission */
-		VERBOSE(CL_VERBOSE_OFF, "unable to open input file");
-		goto err_file;
-	}
-
-	conf->fd = fd;
-
 	/* input info */
 	conf->in_info = (struct input_info_file *) malloc(sizeof(*(conf->in_info)));
 	if (!conf->in_info) {
@@ -160,6 +312,96 @@ int input_init(char *params, void **config)
 
 	/* we don't need this xml tree any more */
 	xmlFreeDoc(doc);
+
+	/* get directory without filename */
+	conf->dir = (char *) malloc(strlen(conf->file) + 1);
+	if (conf->dir == NULL) {
+		VERBOSE(CL_VERBOSE_OFF, "Not enough memory");
+		goto err_file;
+	}
+	strcpy(conf->dir, conf->file);
+	conf->dir = dirname(conf->dir);
+
+	/* get filename without directory */
+	conf->file_copy = (char *) malloc(strlen(conf->file) + 1);
+	if (conf->file_copy == NULL) {
+		VERBOSE(CL_VERBOSE_OFF, "Not enough memory");
+		goto err_file;
+	}
+	strcpy(conf->file_copy, conf->file);
+	conf->filename = basename(conf->file_copy);
+
+	dir = opendir(conf->dir);
+	if (dir == NULL) {
+		VERBOSE(CL_VERBOSE_OFF, "Unable to open input file(s)\n");
+		goto err_file;
+	}
+
+	len = offsetof(struct dirent, d_name) + 
+	          pathconf(conf->dir, _PC_NAME_MAX) + 1;
+
+	entry = (struct dirent *) malloc(len);
+	if (entry == NULL) {
+		VERBOSE(CL_VERBOSE_OFF, "not enough memory");
+		goto err_file;
+	}
+
+	int array_length = NUMBER_OF_INPUT_FILES;
+	input_files = (char **) malloc(array_length * sizeof(char *));
+	if (input_files == NULL) {
+		VERBOSE(CL_VERBOSE_OFF, "Not enough memory");
+		goto err_file;
+	}
+	memset(input_files, 0, NUMBER_OF_INPUT_FILES);
+
+	do {
+		ret = readdir_r(dir, entry, &result);
+		if (ret != 0) {
+			VERBOSE(CL_VERBOSE_OFF, "Error while reading directory %s\n", conf->dir);
+			goto err_file;
+		}
+		
+		if (result == NULL) {
+			/* no more files in directory */
+			break;
+		}
+
+		if ((!strcmp(".", entry->d_name)) || (!strcmp("..", entry->d_name))) {
+			continue;
+		}
+
+		/* check whether this filename matches given regexp */
+		ret = regexp_asterisk(conf->filename, entry->d_name);
+		if (ret == 1) {
+			/* this file matches */
+			if (fcounter >= array_length) {
+				input_files = realloc(input_files, array_length * 2);
+				if (input_files == NULL) {
+					VERBOSE(CL_VERBOSE_OFF, "Not enough memory");
+					goto err_file;
+				}
+				array_length *= 2;
+			}
+
+			input_files[inputf_index] = (char *) malloc(strlen(entry->d_name) + strlen(conf->dir) + 2); /* 2 because of "/" and NULL*/
+			if (input_files[inputf_index] == NULL) {
+				VERBOSE(CL_VERBOSE_OFF, "Not enough memory");
+				goto err_file;
+			}
+			sprintf(input_files[inputf_index], "%s/%s", conf->dir, entry->d_name);
+			inputf_index += 1;
+			fprintf(stderr, "--- DEBUG: file to process: %s\n", input_files[inputf_index-1]);
+		}
+	} while (result);
+
+	conf->input_files = input_files;
+
+
+	ret = prepare_input_file(conf);
+	if (ret == -1) {
+		/* no input files */
+		goto err_file;
+	}
 
 	*config = conf;
 
@@ -181,7 +423,7 @@ err_init:
 }
 
 
-int get_packet(void *config, struct input_info** info, char **packet)
+int get_packet(void *config, struct input_info **info, char **packet)
 {
 	int ret;
 	int counter = 0;
@@ -189,18 +431,18 @@ int get_packet(void *config, struct input_info** info, char **packet)
 	uint16_t packet_len;
 	struct ipfix_config *conf;
 
-
 	conf = (struct ipfix_config *) config;
 
 	*info = (struct input_info *) conf->in_info;
 
 	header = (struct ipfix_header *) malloc(sizeof(*header));
 	if (!header) {
-		VERBOSE(CL_VERBOSE_OFF, "not enough memory");
+		VERBOSE(CL_VERBOSE_OFF, "Not enough memory");
 		return -1;
 	}
 
 	/* read IPFIX header only */
+read_header:
 	ret = read(conf->fd, header, sizeof(*header));
 	if (ret == -1) {
 		if (errno == EINTR) {
@@ -212,15 +454,21 @@ int get_packet(void *config, struct input_info** info, char **packet)
 		goto err_header;
 	}
 	if (ret == 0) {
-		/* EOF */
-		ret = INPUT_CLOSED;
-		goto err_header;
+		/* EOF, next file? */
+		ret = next_file(conf);
+		if (ret == NO_INPUT_FILE) {
+			/* all files processed */
+			ret = INPUT_CLOSED;
+			goto err_header;
+		}
+		/* next file is ready */
+		goto read_header;
 	}
 
 	/* check magic number */
 	if (ntohs(header->version) != IPFIX_VERSION) {
 		/* not an IPFIX file */
-		VERBOSE(CL_VERBOSE_OFF, "input file is not an IPFIX file");
+		VERBOSE(CL_VERBOSE_OFF, "Input file is not an IPFIX file");
 		goto err_header;
 	}
 
@@ -228,14 +476,14 @@ int get_packet(void *config, struct input_info** info, char **packet)
 	packet_len = ntohs(header->length);
 	if (packet_len < sizeof(*header)) {
 		/* invalid length of the IPFIX message */
-		VERBOSE(CL_VERBOSE_OFF, "input file has invalid length (too short)");
+		VERBOSE(CL_VERBOSE_OFF, "Input file has invalid length (too short)");
 		goto err_header;
 	}
 
 	/* allocate memory for whole IPFIX message */
 	*packet = (char *) malloc(packet_len);
 	if (*packet == NULL) {
-		VERBOSE(CL_VERBOSE_OFF, "not enough memory");
+		VERBOSE(CL_VERBOSE_OFF, "Not enough memory");
 		goto err_header;
 	}
 
@@ -254,9 +502,16 @@ int get_packet(void *config, struct input_info** info, char **packet)
 		goto err_info;
 	}
 	if (ret == 0) {
-		/* EOF */
-		ret = INPUT_CLOSED;
-		goto err_info;
+		/* EOF, next file? */
+		ret = next_file(conf);
+		if (ret == NO_INPUT_FILE) {
+			/* all files processed */
+			ret = INPUT_CLOSED;
+			goto err_info;
+		}
+
+		/* TODO fix possible memory leaks */
+		goto read_header;
 	}
 	counter += ret;
 
@@ -277,11 +532,18 @@ err_header:
 int input_close(void **config)
 {
 	struct ipfix_config *conf = *config;
-	int ret;
+	int ret = 0;
+	int i;
 
-	ret = close(conf->fd);
-	if (ret == -1) {
-		VERBOSE(CL_VERBOSE_OFF, "error while closing output file");
+	free(conf->dir);
+	free(conf->file_copy);
+
+	/* free list of input files */
+	if (conf->input_files) {
+		for (i = 0; conf->input_files[i]; i++) {
+			free(conf->input_files[i]);
+		}
+		free(conf->input_files);
 	}
 
 	xmlFree(conf->xml_file);
