a1dcb4ab3d0b6634825fbaf858c98b3c30107fee
Michal Kozubík <xkozub03@stud.fit.vutbr.cz>
2013-11-07 15:29:14 +0100

    fix previous commit

--- tools/fbitdump/src/Filter.cpp
+++ tools/fbitdump/src/Filter.cpp
@@ -133,7 +133,7 @@ time_t Filter::parseTimestamp(std::string str) const throw (std::invalid_argumen
 	if (strptime(str.c_str(), "%Y/%m/%d.%H:%M:%S", &ctime) == NULL) {
 		throw std::invalid_argument(std::string("Cannot parse timestamp '") + str + "'");
 	}
-	ctime.tm_isdst = 1;
+	ctime.tm_isdst = 0;
 
 	return mktime(&ctime);
 }
