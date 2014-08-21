4586c5d17ac6537e2c7077fa27da64e8e02fc927
Jan Remeš <xremes00@stud.fit.vutbr.cz>
2014-08-21 16:22:45 +0200

    fbitdump: Added SMTP status code plugin

---
+++ tools/fbitdump/src/plugins/smtp_statuscode.c
@@ -0,0 +1,172 @@
+/*
+ * File:    smtp_statuscode.c
+ * Author:  Jan Remes (xremes00@stud.fit.vutbr.cz)
+ * Project: ipfixcol
+ *
+ * Description: This file contains fbitdump plugin for displaying SMTP status
+ *              code field in text format
+ *
+ * (C) CESNET 2014
+ */
+
+
+/**** INCLUDES and DEFINES *****/
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <string.h>
+
+#include "plugin_header.h"
+
+//#define DBG(str) do { fprintf(stderr, "DEBUG: %s() in %s: %s\n", __func__, __FILE__, str); } while(0)
+#define DBG(str)
+
+#define SEP_CHAR '|'
+#define DEFAULT_CHAR '-'
+#define SC_UNKNOWN 0x80000000
+
+/***** TYPEDEFs and DATA *****/
+
+typedef struct {
+    unsigned position;
+    char flag;
+} item_t;
+
+static const item_t values[] = {
+    {  0, 'S' }, // 211 System status, or system help reply
+    {  1, 'H' }, // 214 Help message
+    {  2, 'R' }, // 220 <domain> Service ready
+    {  3, 'C' }, // 221 <domain> Service closing transmission channel
+    {  4, 'O' }, // 250 Requested mail action okay, completed
+    {  5, 'U' }, // 251 User not local; will forward to <forward-path>
+    {  6, 'V' }, // 252 Cannot VRFY user, but will accept message and attempt delivery
+    /* Vertical bar will be here */
+    {  8, 'I' }, // 354 Start mail input; end with <CRLF>.<CRLF>
+    /* Vertical bar will be here */
+    { 10, 'N' }, // 421 Service not available, closing transmission channel
+    { 11, 'M' }, // 450     Requested mail action not taken: mailbox unavailable
+    { 12, 'L' }, // 451     Requested action aborted: local error in processing
+    { 13, 'S' }, // 452     Requested action not taken: insufficient system storage
+    { 14, 'P' }, // 455     Cannot accomodate parameter (http://tools.ietf.org/html/rfc1869)
+    /* Vertical bar will be here */
+    { 16, 'C' }, // 500     Syntax error, command unrecognised
+    { 17, 'A' }, // 501     Syntax error in parameters or arguments
+    { 18, 'I' }, // 502     Command not implemented
+    { 19, 'S' }, // 503     Bad sequence of commands
+    { 20, 'P' }, // 504     Command parameter not implemented
+    { 21, 'M' }, // 550     Requested action not taken: mailbox unavailable
+    { 22, 'U' }, // 551     User not local; please try <forward-path>
+    { 23, 'E' }, // 552     Requested mail action aborted: exceeded storage allocation
+    { 24, 'N' }, // 553     Requested action not taken: mailbox name not allowed
+    { 25, 'F' }, // 554     Transaction failed
+    { 26, 'R' }, // 555     Cannot recognize or implement parameter (http://tools.ietf.org/html/rfc1869)
+    /* Vertical bar will be here */
+};
+
+static const int NAMES_SIZE = sizeof(values) / sizeof(item_t);
+
+// Get room for:
+// - 4 separators
+// - Unknown flag
+// - null byte
+#define FLAGSIZE (NAMES_SIZE + 6)
+
+
+/***** FUNCTIONS *****/
+
+static int is_sep_position(unsigned i)
+{
+    switch(i)
+    {
+        case 7:
+        case 9:
+        case 15:
+        case 27:
+            return 1;
+        default:
+            return 0;
+    }
+}
+
+/**
+ * Check for buffer's size
+ */
+int init(void)
+{
+    DBG("called");
+
+    if(PLUGIN_BUFFER_SIZE < FLAGSIZE)
+        return 1;
+    else return 0;
+}
+
+/**
+ * Fill the buffer with text representation of field's content
+ */
+void format( const union plugin_arg * arg,
+                int plain_numbers,
+                char buffer[PLUGIN_BUFFER_SIZE] )
+{
+    DBG("called");
+
+    // get rid of the warning
+    if(plain_numbers) {;}
+
+    int i;
+
+    // Initializing the field
+    for(i = 0; i < FLAGSIZE - 1; i++)
+    {
+        if(is_sep_position(i))
+        {
+            buffer[i] = SEP_CHAR;
+        }
+        else buffer[i] = DEFAULT_CHAR;
+    }
+    buffer[FLAGSIZE - 1] = '\0';
+
+    // Filling in flag values
+    for(i = 0; i < NAMES_SIZE; i++)
+    {
+        if(arg->uint32 & (((uint32_t)1) << i))
+        {
+            buffer[values[i].position] = values[i].flag;
+        }
+    }
+
+    if(arg->uint32 & SC_UNKNOWN)
+    {
+        buffer[FLAGSIZE - 2] = 'U';
+    }
+}
+
+/**
+ * Parse text data and return inner format
+ */
+void parse(char *input, char out[PLUGIN_BUFFER_SIZE])
+{
+    int i;
+    int offset = 0;
+    uint32_t result = 0;
+
+    for(i = 0; i < NAMES_SIZE; i++)
+    {
+        if(is_sep_position(i))
+        {
+            offset++;
+            continue;
+        }
+
+        if(input[i+offset] == values[i].flag)
+        {
+            result |= ((uint32_t)1) << i;
+        }
+    }
+
+    if(input[FLAGSIZE - 2] == 'U')
+    {
+        result |= SC_UNKNOWN;
+    }
+
+    snprintf(out, PLUGIN_BUFFER_SIZE, "%u", result);
+}
