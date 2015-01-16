78ae82642cc38d203b6fdef609e33240606de91a
Kirc <kirc@secdorks.net>
2015-01-16 02:04:18 +0100

    queues.c: Fixed missing unlock (99844)

--- base/src/queues.c
+++ base/src/queues.c
@@ -266,6 +266,11 @@ int rbuffer_remove_reference (struct ring_buffer* rbuffer, unsigned int index, i
 			if (rbuffer->count == 0) {
 				if (pthread_cond_signal (&(rbuffer->cond_empty)) != 0) {
 					MSG_ERROR(msg_module, "Condition signal failed (%s:%d)", __FILE__, __LINE__);
+
+					if (pthread_mutex_unlock (&(rbuffer->mutex)) != 0) {
+						MSG_ERROR(msg_module, "Mutex unlock failed (%s:%d)", __FILE__, __LINE__);
+					}
+					
 					return (EXIT_FAILURE);
 				}
 			}
