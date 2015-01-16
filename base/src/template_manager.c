522159302107f3cb2ec819d2d1353d144c13e7fd
Kirc <kirc@secdorks.net>
2015-01-16 02:01:42 +0100

    template_manager.c: Fixed deference after null check (99842)

--- base/src/template_manager.c
+++ base/src/template_manager.c
@@ -296,6 +296,7 @@ struct ipfix_template *tm_create_template(void *template, int max_len, int type,
 	/* allocate memory for new template */
 	if ((new_tmpl = malloc(tmpl_length)) == NULL) {
 		MSG_ERROR(msg_module, "Memory allocation failed (%s:%d)", __FILE__, __LINE__);
+		return NULL;
 	}
 
 	if (tm_fill_template(new_tmpl, template, tmpl_length, data_length, type, odid) == 1) {
