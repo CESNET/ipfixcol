CESNET/ipfixcol

commit 29c5729802dbcea1fb1683c2fda9d55123e4c1de

/input/tcp/tcp_input.c: Fixed resource leaks (99903)

base/src/input/tcp/tcp_input.c
@@ -364,6 +364,7 @@ void *input_listen(void *config)
         /* unset the address so that we do not free it incidentally */
         address = NULL;
 
+        close(new_sock);
         pthread_cleanup_pop(0);
     }
     return NULL;
@@ -505,8 +506,10 @@ int input_init(char *params, void **config)
             } else if (xmlStrEqual(cur_node->name, BAD_CAST "optionsTemplateLifePacket")) {
                 conf->info.options_template_life_packet = tmp_val;
             } else { /* unknown parameter, ignore */
-                free(tmp_val);
+                // Do nothing
             }
+
+            free(tmp_val);
         }
     }
 

commit 4a62ceb0ca39197543d555bc013dc693ee8db8c9

Fixed incorrectly closed socket in tcp_input.c introduced in 29c57298

base/src/input/tcp/tcp_input.c
@@ -364,7 +364,6 @@ void *input_listen(void *config)
         /* unset the address so that we do not free it incidentally */
         address = NULL;
 
-        close(new_sock);
         pthread_cleanup_pop(0);
     }
     return NULL;