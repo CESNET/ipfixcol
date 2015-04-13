21ccbba9994e5d96f70862c4218614e79a5a9204
Kirc <kirc@secdorks.net>
2015-04-13 16:40:36 +0200

    create_internal.sh: create 'configs' dir if nonexistent

--- base/tests/ipfixcol_test/create_internal.sh
+++ base/tests/ipfixcol_test/create_internal.sh
@@ -37,12 +37,17 @@
 #
 
 CREATE_BASE="$PWD"
+CONFIGS_DIR="configs"
 INTERNAL_ORIG="../../config/internalcfg.xml"
-INTERNAL="configs/internalcfg.xml"
+INTERNAL="$CONFIGS_DIR/internalcfg.xml"
 
 if [ ! -f $INTERNAL_ORIG ]; then
 	echo "Missing $INTERNAL_ORIG"
 	exit 1
+fi
+
+if [ ! -d $CONFIGS_DIR ]; then
+	mkdir $CONFIGS_DIR
 fi
 
 cp $INTERNAL_ORIG $INTERNAL
