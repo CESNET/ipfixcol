43f9607710231d1a386cc325075f03dd0501619a
Lukáš Huták <xhutak01@stud.fit.vutbr.cz>
2016-02-11 13:53:20 +0100

    ipfixsend: changed the behavior of the speed limit packets/s

--- base/src/utils/ipfixsend/sender.c
+++ base/src/utils/ipfixsend/sender.c
@@ -1,6 +1,7 @@
 /**
  * \file sender.c
  * \author Michal Kozubik <kozubik@cesnet.cz>
+ * \author Lukas Hutak <hutak@cesnet.cz>
  * \brief Functions for parsing IP, connecting to collector and sending packets
  *
  * Copyright (C) 2015 CESNET, z.s.p.o.
@@ -42,7 +43,6 @@
 #include <stdlib.h>
 #include <netdb.h>
 #include <string.h>
-#include <unistd.h>
 #include <time.h>
 
 #include <sys/socket.h>
@@ -62,6 +62,8 @@
 #define UDP_MTU MSG_MAX_LENGTH - 535
 #define MIN(_first_, _second_) ((_first_) < (_second_)) ? (_first_) : (_second_)
 
+#define MAX_SLEEP 1000000L // 1 second in miliseconds
+
 static int stop_sending = 0;
 static struct timeval begin = {0};
 
@@ -79,41 +81,76 @@ int send_packet(sisoconf *sender, char *packet)
 }
 
 /**
+ * \brief Calculate time difference
+ * \param[in] start First timestamp
+ * \param[in] end Second timestamp
+ * \return Number of microseconds between timestamps
+ */
+long timeval_diff(const struct timeval *start, const struct timeval *end)
+{
+	return (end->tv_sec - start->tv_sec) * 1000000L
+		+ (end->tv_usec - start->tv_usec);
+}
+
+/**
  * \brief Send all packets from array
  */
 int send_packets(sisoconf *sender, char **packets, int packets_s)
 {
-    int i, ret;
 	struct timeval end;
-	double ellapsed;
-	
-	/* These must be static variables - local would be rewrited with each call of send_packets */
+	struct timespec sleep_time = {0};
+
+	/* These must be static variables - local would be rewrited with each call
+	 * of send_packets */
 	static int pkts_from_begin = 0;
-	
-    for (i = 0; packets[i] && stop_sending == 0; ++i) {
-        /* send packet */
-        ret = send_packet(sender, packets[i]);
+	static double time_per_pkt = 0.0; // [ms]
+
+	if (begin.tv_sec == 0) {
+		/* Absolutely first packet */
+		gettimeofday(&begin, NULL);
+		time_per_pkt = 1000000.0 / packets_s; // [ms]
+	}
+
+	for (int i = 0; packets[i] && stop_sending == 0; ++i) {
+		/* send packet */
+		int ret = send_packet(sender, packets[i]);
         if (ret != SISO_OK) {
             return SISO_ERR;
         }
-		
+
 		pkts_from_begin++;
-		/* packets counter reached, sleep? */
-		if (packets_s > 0 && (pkts_from_begin >= packets_s || begin.tv_sec == 0)) {
-			/* end of sending interval in microseconds */
-			gettimeofday(&end, NULL);
-			
-			/* Should sleep? */
-			ellapsed = end.tv_usec - begin.tv_usec;
-			if (ellapsed < 1000000.0) {
-				usleep(1000000.0 - ellapsed);
-				gettimeofday(&end, NULL);
-			}
-			
-			begin = end;
+		if (packets_s <= 0) {
+			/* Limit for packets/s is not enabled */
+			continue;
+		}
+
+		/* Calculate expected time of sending next packet */
+		gettimeofday(&end, NULL);
+		long elapsed = timeval_diff(&begin, &end);
+		if (elapsed < 0) {
+			/* Should be never negative. Just for sure... */
+			elapsed = pkts_from_begin * time_per_pkt;
+		}
+
+		long next_start = pkts_from_begin * time_per_pkt;
+		long diff = next_start - elapsed;
+
+		if (diff >= MAX_SLEEP) {
+			diff = MAX_SLEEP - 1;
+		}
+
+		/* Sleep */
+		if (diff > 0) {
+			sleep_time.tv_nsec = diff * 1000L;
+			nanosleep(&sleep_time, NULL);
+		}
+
+		if (pkts_from_begin >= packets_s) {
+			/* Restart counter */
+			gettimeofday(&begin, NULL);
 			pkts_from_begin = 0;
 		}
-    }
-	
+	}
+
     return 0;
 }
