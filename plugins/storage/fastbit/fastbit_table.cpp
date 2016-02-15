CESNET/ipfixcol

commit 843803ea60cbb1ad9a1befcb4304be6b2b5ed549

Fastbit storage: minor tweaks in -part.txt

plugins/storage/fastbit/fastbit_table.cpp
@@ -109,13 +109,13 @@ int template_table::update_part(std::string path)
 	}
 
 	/* Insert header */
-	ss << "BEGIN HEADER";
-	ss << "\nName = " << std::string(this->_name);
-	ss << "\nDescription = Generated by FastBit plugin for IPFIXcol";
-	ss << "\nNumber_of_rows = " << _rows_in_window + rows_in_part;
-	ss << "\nNumber_of_columns = " << elements.size();
-	ss << "\nTimestamp = " << time(NULL);
-	ss << "\nEND HEADER\n\n";
+	ss << "BEGIN HEADER\n";
+	ss << "Name=" << std::string(this->_name) << "\n";
+	ss << "Description=Generated by FastBit plugin for IPFIXcol\n";
+	ss << "Number_of_rows=" << _rows_in_window + rows_in_part << "\n";
+	ss << "Number_of_columns=" << elements.size() << "\n";
+	ss << "Timestamp=" << time(NULL) << "\n";
+	ss << "END HEADER\n";
 
 	/* Insert row info */
 	for (el_it = elements.begin(); el_it != elements.end(); ++el_it){

plugins/storage/fastbit/fastbit_element.cpp
@@ -190,10 +190,10 @@ int element::flush(std::string path)
 
 std::string element::get_part_info()
 {
-	return std::string("\nBegin Column") + \
-		"\nname = " + std::string(this->_name) + \
-		"\ndata_type = " + ibis::TYPESTRING[(int) this->_type] + \
-		"\nEnd Column\n";
+	return std::string("\nBEGIN Column\n") \
+		+ "name=" + std::string(this->_name) + "\n" \
+		+ "data_type=" + ibis::TYPESTRING[(int) this->_type] + "\n" \
+		+ "END Column\n";
 }
 
 el_var_size::el_var_size(int size, uint32_t en, uint16_t id, uint32_t buf_size)

commit 98765e4bc15fa565bae49ad3f180230f57d75958

Fixed broken fbit plugin (incorrect read from -part.txt file introduced in 843803ea)

plugins/storage/fastbit/fastbit_table.cpp
@@ -45,7 +45,7 @@ extern "C" {
 
 #include "fastbit_table.h"
 
-#define ROW_LINE "Number_of_rows ="
+#define ROW_LINE "Number_of_rows="
 
 uint64_t get_rows_from_part(const char *part_path)
 {