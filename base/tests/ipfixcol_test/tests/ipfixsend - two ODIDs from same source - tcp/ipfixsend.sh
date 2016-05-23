1617ffa7ce1f7a23fa9d304a7133bfbeea8135c2
Petr Velan <petr.velan@cesnet.cz>
2016-05-23 14:50:15 +0200

    Fixed test ipfixsend - two ODIDs from same source - tcp

--- base/tests/ipfixcol_test/tests/ipfixsend - two ODIDs from same source - tcp/ipfixsend.sh
+++ base/tests/ipfixcol_test/tests/ipfixsend - two ODIDs from same source - tcp/ipfixsend.sh
@@ -1,3 +1,3 @@
-$IPFIX_TEST_IPFIXSEND -i ../ipfix_data/04-odid256,512.ipfix -d 127.0.0.1 -p 4739 -t udp -n 1 -S 25
+$IPFIX_TEST_IPFIXSEND -i ../ipfix_data/04-odid256,512.ipfix -d 127.0.0.1 -p 4739 -t tcp -n 1 -S 25
 
 wait $!
